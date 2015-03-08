73f1f7fc7811b8e51f1ae54831a6d7d957c3368a
Sean Prasertsit <sean@prasertsit.com>
2015-03-07 17:29:30 -0800

    Fixed menu options and grabbed all initial database inputs

--- src/edu/ucsb/cs/main.java
+++ src/edu/ucsb/cs/main.java
@@ -1,6 +1,9 @@
 package edu.ucsb.cs;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.Date;
 
 /**
  * Created by sean on 3/4/15.
@@ -8,24 +11,55 @@ import java.sql.*;
 public class main {
 
     public void main(String[] args) {
-        menu();
+        displayMenu();
     }
 
-    public void menu() {
+    public void displayMenu() {
+
+        // Set write to console and read from console
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Welcome to the Health Information System");
-        System.out.println("Who would you like to log in as? Please enter a number.");
-        System.out.println("Administrator: 0");
-        System.out.println("Doctor: 1");
-        System.out.println("Patient: 2");
+        System.out.println("Who would you like to log in as? Please enter a number or 0 to exit program.");
+        System.out.println("Administrator: 1");
+        System.out.println("Doctor: 2");
+        System.out.println("Patient: 3");
         System.out.println("Log in as: ");
+
+        // Select user
+        String menuInput = in.next();
+        if(menuInput.equals("0")) {
+            System.out.println("Exiting HIS");
+            return;
+        }
+        else if(menuInput.equals("1")) {
+            adminConsole();
+        }
+        else if(menuInput.equals("2")) {
+            doctorConsole();
+        }
+        else if(menuInput.equals("3")) {
+            patientConsole();
+        }
+        else {
+            System.out.println("Error; exiting");
+            return;
+        }
     }
 
-    public void readDb(String source, String dest, String table) {
+    // Get the current date and time for whatever needs it
+    public String getDate() {
+        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date());
+        return date;
+    }
+
+    // Read everything from the input file and put it in the database
+    public void initDb(String source, String dest, String table) {
 
         // Set some connection variables to null in case
         Connection connection = null;
         Statement statement = null;
-        ResultSet rSet = null;
+        ResultSet resultSet = null;
         Connection destDb = null;
 
         try {
@@ -41,7 +75,7 @@ public class main {
             statement = connection.createStatement();
 
             // Get result of query
-            rSet = statement.executeQuery("select * from " + source + "." + table);
+            resultSet = statement.executeQuery("select * from " + source + "." + table);
 
             // Connect to destination database
             destDb = DriverManager.getConnection(
@@ -53,5 +87,93 @@ public class main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        // Get every single input
+        try {
+            while(resultSet.next()) {
+
+                // Get metadata
+                String lastAccessed = resultSet.getString("Last_Accessed");
+
+                // Get patient info
+                String patientId    = resultSet.getString("patientId");
+                String givenName    = resultSet.getString("GivenName");
+                String familyName   = resultSet.getString("FamilyName");
+                String birthTime    = resultSet.getString("BirthTime");
+                String providerId   = resultSet.getString("providerId");
+
+                // Get guardian info
+                String guardianNo   = resultSet.getString("GuardianNo");
+                String relationship = resultSet.getString("Relationship");
+                String firstName    = resultSet.getString("FirstName");
+                String lastName     = resultSet.getString("LastName");
+                String phone        = resultSet.getString("phone");
+                String address      = resultSet.getString("address");
+                String city         = resultSet.getString("city");
+                String state        = resultSet.getString("state");
+                String zip          = resultSet.getString("zip");
+
+                // Get author info
+                String authorId             = resultSet.getString("AuthorId");
+                String authorTitle          = resultSet.getString("AuthorTitle");
+                String authorFirstName      = resultSet.getString("AuthorFirstName");
+                String authorLastName       = resultSet.getString("AuthorLastName");
+                String participatingRole    = resultSet.getString("ParticipatingRole");
+
+                // Get insurance info
+                String payerId      = resultSet.getString("PayerId");
+                String name         = resultSet.getString("Name");
+                String policyHolder = resultSet.getString("PolicyHolder");
+                String policyType   = resultSet.getString("PolicyType");
+                String purpose      = resultSet.getString("Purpose");
+
+                // Get family history info
+                String relativeId   = resultSet.getString("RelativeId");
+                String relation     = resultSet.getString("Relation");
+                String age          = resultSet.getString("age");
+
+                // Get allergy info
+                String diagnosis    = resultSet.getString("Diagnosis");
+                String id           = resultSet.getString("Id");
+                String substance    = resultSet.getString("Substance");
+                String reaction     = resultSet.getString("Reaction");
+                String status       = resultSet.getString("Status");
+
+                // Get lab test info
+                String labTestResultId      = resultSet.getString("LabTestResultId");
+                String patientVisitId       = resultSet.getString("PatientVisitId");
+                String labTestPerformedDate = resultSet.getString("LabTestPerformedDate");
+                String labTestType          = resultSet.getString("LabTestType");
+                String testResultValue      = resultSet.getString("TestResultValue");
+                String referenceRangeHigh   = resultSet.getString("ReferenceRangeHigh");
+                String referenceRangeLow    = resultSet.getString("ReferenceRangeLow");
+
+                // Get plan info
+                String planId           = resultSet.getString("PlanId");
+                String activity         = resultSet.getString("Activity");
+                String scheduledDate    = resultSet.getString("ScheduledDate");
+
+                // Get missing info
+                String currentDate = getDate();
+                String suffix = "";
+                String gender = "";
+                String patientRole = "";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void adminConsole() {
+        return;
+    }
+
+    public void doctorConsole() {
+        return;
+    }
+
+    public void patientConsole() {
+        return;
     }
 }
